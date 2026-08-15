9e1b8e843dc951013d5da90d1f8c3d1253437ba4
k04ae <kaylebdoculan2012@gmail.com>
2026-08-15 17:41:05 +0800

    Update q1_sg2_a3.py

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -1,1 +1,48 @@
+birth_year = int(input("Enter your birth year: ")
 
+if birth_year < 1900:
+                 print("Invalid Year, it should not be earlier than 1900")
+
+except ValueError:
+                 exit()
+
+zodiac_sign = (birth_year - 1900) % 12
+
+if zodiac sign == 0:
+  print("Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
+
+if zodiac_sign == 1:
+  print("Your Chinese Zodiac Sign is : Ox (牛 / Niú)")
+
+if zodiac_sign == 2:
+  print("Your Chinese Zodiac Sign is : Tiger (虎 / Hǔ)")
+
+if zodiac_sign == 3:
+  print("Your Chinese Zodiac Sign is : Rabbit (兔 / Tù)")
+
+if zodiac_sign == 4:
+  print("Your Chinese Zodiac Sign is : Dragon (龙 / Lóng)")
+
+if zodiac_sign == 5:
+  print("Your Chinese Zodiac Sign is : Snake (蛇 / Shé)")
+
+if zodiac_sign == 6:
+  print("Your Chinese Zodiac Sign is : Horse (马 / Mǎ)")
+
+if zodiac_sign == 7:
+  print("Your Chinese Zodiac Sign is : Goat (羊 / Yáng)")
+
+if zodiac_sign == 8:
+  print("Your Chinese Zodiac Sign is : Monkey (猴 / Hóu)")
+
+if zodiac_sign == 9:
+  print("Your Chinese Zodiac Sign is : Pig (猪 / Zhū)")
+
+if zodiac_sign == 10:
+  print("Your Chinese Zodiac Sign is : Dog (狗 / Gǒu)")
+
+if zodiac_sign == 11:
+  print("Your Chinese Zodiac Sign is : Rat (鼠 / Shǔ)")
+
+
+  
